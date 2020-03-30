a3ad5333c0e7c7d8238569bdd58ce0250fcefb31
Attilio Carotenuto <carotenuto.attilio@gmail.com>
2020-03-30 21:35:11 +0200

    Post-processing examples

---
+++ Assets/HimekiShadersCollection/Examples/Common/Scripts/MaterialValueSlider.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class MaterialValueSlider : MonoBehaviour
+{
+	public string propertyName;
+    public Slider valueSlider;
+    public Text valueText;
+    public Material material;
+
+    void OnEnable()
+    {
+        valueSlider.value = material.GetFloat(propertyName);
+
+        valueText.text = valueSlider.value.ToString();
+    }
+    public void onSliderValueChanged(float value)
+    {
+        material.SetFloat(propertyName, value);
+
+        valueText.text = value.ToString();
+    }
+}
+
+
