5b615d6daeb883db2396a58655964cd0b1f12cb6
Attilio Carotenuto <carotenuto.attilio@gmail.com>
2020-03-31 00:32:48 +0200

    Added simple camera controls to examples

---
+++ Assets/HimekiShadersCollection/Examples/Common/Scripts/SimpleCameraControls.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+
+public class SimpleCameraControls : MonoBehaviour
+{
+    public float cameraSpeed = 10f;
+    public float mouseSensitivity = 0.15f;
+	
+    private Vector3 lastDragPosition;
+    private bool draggingMouse = false;
+
+    void Update()
+    {
+        UpdateMouseDrag();
+        UpdateTranslation();
+    }
+
+    private void UpdateTranslation()
+    {
+        Vector3 input = new Vector3(Input.GetAxis("Horizontal"), 0f, Input.GetAxis("Vertical"));
+        Vector3 translation = input * cameraSpeed * Time.deltaTime;
+        transform.Translate(translation);
+    }
+
+    private void UpdateMouseDrag()
+    {
+        var isMousePressed = Input.GetMouseButton(0);
+
+        if (!draggingMouse && isMousePressed)
+        {
+            lastDragPosition = Input.mousePosition;
+            draggingMouse = true;
+        }
+        else
+        {
+            if (isMousePressed)
+            {
+                var mouseMovement = (Input.mousePosition - lastDragPosition) * mouseSensitivity;
+                var rotationAngles = new Vector3(-mouseMovement.y, mouseMovement.x, 0f);
+                transform.eulerAngles = new Vector3(transform.eulerAngles.x + rotationAngles.x, transform.eulerAngles.y + rotationAngles.y, 0f);
+
+                lastDragPosition = Input.mousePosition;
+            }
+            else
+            {
+                draggingMouse = false;
+            }
+        }
+    }
+
+}
